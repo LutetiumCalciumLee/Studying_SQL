a35d72fdebc9959f70f8c2130c6d359f57cea669
LutetiumCalcium <2501340038lee@gmail.com>
2025-05-20 08:54:21 +0900

    Delete 2_15.sql

--- 2_15.sql
+++
@@ -1,9 +0,0 @@
-SELECT name , tel , 
-    DECODE (SUBSTR(tel, 1, 3),
-              '02)',  'SEOUL',
-              '031',  'GYEONGGI',
-              '051',  'BUSAN',
-              '052',  'ULSAN',
-              '055',  'GYEONGNAM') "LOC"
-FROM student
-WHERE deptno1=101;
